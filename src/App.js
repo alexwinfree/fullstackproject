f29471e9ab109308fe4781bf728bbf1b0df5731e
Alex Winfree <alex.winfree@Alex.local>
2019-07-21 12:25:53 -0600

    first upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
 
 function App() {
   return (
     <div className="App">
+      <h1> 
+      <button className= "search">  
+        <input type= "text" placeholder=" Search here"></input>
+      </button>
+
+        <button className= "LogIn">
+          Log in
+          </button>
+
+          <button className= "UserName">
+            <input type="text" placeholder= "UserName"></input>            
+          </button>
+
+          <button className= "Password">
+            <input type="text" placeholder= "Password"></input>            
+          </button>
+      </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and reload! 
         </p>
         <a
           className="App-link"
@@ -19,6 +37,7 @@ function App() {
           Learn React
         </a>
       </header>
+     
     </div>
   );
 }
---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function MyComponent() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default MyComponent;
