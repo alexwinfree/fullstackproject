06e915f2eb1e26d1bd27afa225faf8f53ddf011a
Alex Winfree <alex.winfree@Alex.local>
2019-07-23 14:10:23 -0600

    header bar/logo Co-authored-by: juanifit <juang@ifit.com>

--- src/App.js
+++ src/App.js
@@ -6,39 +6,31 @@ import MyComponent from './MyComponent';
 function App() {
   return (
     <div className="App">
-      <h1> 
-      <button className= "Search">  
-        <input type= "text" placeholder=" Search here"></input>
-      </button>
-
-        <button className= "LogIn">
-          Log in
-          </button>
-
-          <button className= "UserName">
-            <input type="text" placeholder= "UserName"></input>            
-          </button>
-
-          <button className= "Password">
-            <input type="text" placeholder= "Password"></input>            
-          </button>
-
-          <img src={logo} className="App-logo" alt="logo" />
-      </h1>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and reload! 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header className="header">
+        <div class="reddit-icon">
+        <a aria-label="Home" href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <g><circle fill="#FF4500" cx="10" cy="10" r="10"></circle>
+            <path fill="#FFF" d="M16.67,10A1.46,1.46,0,0,0,14.2,9a7.12,7.12,0,0,0-3.85-1.23L11,4.65,13.14,5.1a1,1,0,1,0,.13-0.61L10.82,4a0.31,0.31,0,0,0-.37.24L9.71,7.71a7.14,7.14,0,0,0-3.9,1.23A1.46,1.46,0,1,0,4.2,11.33a2.87,2.87,0,0,0,0,.44c0,2.24,2.61,4.06,5.83,4.06s5.83-1.82,5.83-4.06a2.87,2.87,0,0,0,0-.44A1.46,1.46,0,0,0,16.67,10Zm-10,1a1,1,0,1,1,1,1A1,1,0,0,1,6.67,11Zm5.81,2.75a3.84,3.84,0,0,1-2.47.77,3.84,3.84,0,0,1-2.47-.77,0.27,0.27,0,0,1,.38-0.38A3.27,3.27,0,0,0,10,14a3.28,3.28,0,0,0,2.09-.61A0.27,0.27,0,1,1,12.48,13.79Zm-0.18-1.71a1,1,0,1,1,1-1A1,1,0,0,1,12.29,12.08Z"></path>
+            </g>
+          </svg>
         </a>
+        </div>
+
+        <div className="move-to-right">
+          <button className="header-button">
+            Log in
+          </button>
+
+          <button className="header-button">
+            Signup
+          </button>
+        </div>
+
+
       </header>
-     
+
+
     </div>
   );
 }
